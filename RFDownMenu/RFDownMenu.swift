4fb1013b5cfcccfe9ee75e9b175392bfe65f0d6d
冯剑 <fengjian0213@126.com>
2019-04-26 18:00:31 +0800

    create

---
+++ RFDownMenu/RFDownMenu.swift
@@ -0,0 +1,102 @@
+//
+//  RFDownMenu.swift
+//  RFDownMenu
+//
+//  Created by 冯剑 on 2019/4/26.
+//  Copyright © 2019 aomiao. All rights reserved.
+//
+
+import UIKit
+
+class RFDownMenu: UIView {
+    lazy var blackBg:UIView={
+        let v = UIButton(frame: UIScreen.main.bounds)
+        v.backgroundColor = .black
+        v.alpha = 0.3
+        v.addTarget(self, action: #selector(closeItemView), for: .touchUpInside)
+        return v
+    }()
+
+    var needShow=true
+    var btnArray = [UIButton]()
+    var currentBtn:UIButton?
+    var itemViews = [UIView]()
+    var titles:[String]!{
+        didSet{
+            setTitleBtns()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .yellow
+        
+    }
+    
+    func setTitleBtns(){
+        let btnW = NSInteger(UIScreen.main.bounds.width) / titles.count
+        for  i in 0..<titles.count{
+            let btn = UIButton(frame: CGRect(x: i*btnW , y:0, width: btnW, height: Int(self.frame.size.height)))
+            btn.setTitleColor(.black, for: .normal)
+            btn.setTitle(titles[i], for: .normal)
+            addSubview(btn)
+            btn.addTarget(self, action: #selector(clickItemTitle), for: .touchUpInside)
+            btnArray.append(btn)
+        }
+    }
+    
+    @objc func closeItemView(){
+        clickItemTitle(btn: currentBtn!)
+    }
+    
+    @objc func clickItemTitle(btn:UIButton){
+        let clickIndex = btnArray.index(of:btn)!
+        
+        if currentBtn == btn {
+            showItemViewTag(show: needShow, view: itemViews[clickIndex])
+            return
+        }
+        
+        for v:UIView in itemViews{
+            v.removeFromSuperview()
+        }
+        
+        needShow = true
+        let currentView = itemViews[clickIndex]
+        showItemViewTag(show: needShow, view: currentView)
+        currentBtn = btn
+    }
+
+    func showItemViewTag(show:Bool, view:UIView){
+        if  show {
+            self.superview?.addSubview(blackBg)
+            self.superview?.addSubview(view)
+            self.superview?.bringSubviewToFront(self)
+            UIView.animate(withDuration: 0.2, animations: {
+                self.blackBg.alpha = 0.3
+                var f = view.frame
+                f.origin.y = self.frame.origin.y+self.frame.size.height
+                view.frame = f
+                
+            }) { (_) in
+                self.needShow = false
+            }
+        }else{
+            UIView.animate(withDuration: 0.2, animations: {
+                self.blackBg.alpha = 0
+                var f = view.frame
+                f.origin.y = -f.size.height
+                view.frame = f
+            }) { (_) in
+                self.needShow = true
+                self.blackBg.removeFromSuperview()
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- RFDownMenu/ViewController.swift
+++ RFDownMenu/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 300))
+        view1.backgroundColor = UIColor.magenta
+        let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 100))
+        view2.backgroundColor = UIColor.cyan
+        let view3 = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 200))
+        view3.backgroundColor = UIColor.brown
+        
+        
+        let menu = RFDownMenu(frame:CGRect(x: 0, y: 80, width: 375, height: 50))
+        menu.titles = ["1111","2222","333"]
+        menu.itemViews = [view1, view2, view3]
+        view.addSubview(menu)
+        
     }
 
 
